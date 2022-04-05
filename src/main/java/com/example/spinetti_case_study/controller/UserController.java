7d89192ca1a724903a03e58bbeacd41cf3ec70e3
Dominick Spinetti <spinettidominick@gmail.com>
2022-04-05 14:21:55 -0700

    Fixed redirect issue with deleteUser mapping

--- src/main/java/com/example/spinetti_case_study/controller/UserController.java
+++ src/main/java/com/example/spinetti_case_study/controller/UserController.java
@@ -25,19 +25,19 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+      //Mapping for login page
     @GetMapping("/")
     public String getAllUsers(Model model) {
         return "Login";
     }
-
+      //Creating model for account creation
     @GetMapping("/showCreateAccount")
     public String showCreateAccount(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         return "CreateAccount";
     }
-
+     // Saving new user to the database with binding result handling backend form validation
     @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute("user") @Valid User user, BindingResult bindingresult) {
 
@@ -48,27 +48,27 @@ public class UserController {
         userService.saveUser(user);
         return "redirect:/";
     }
-
+     //Mapping for update account
     @GetMapping("/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "UpdateAccount";
     }
-
+      //mapping for listing accounts
     @GetMapping("/listAccounts")
     public String getAllUsersList(Model model) {
         model.addAttribute("listUsers", userService.getAllUsers());
         return "ListAccounts";
     }
-
+     //Mapping for deleting a user
     @GetMapping("deleteUser/{id}")
     public String deleteUser(@PathVariable(value = "id") long id) {
 
         this.userService.deleteUserById(id);
-        return "ListAccounts";
+        return "redirect:/listAccounts";
     }
-
+      //Mapping for main home page
     @GetMapping("/showHomeScreen")
     public String showHomeScreen(Model model) {
         return "HomeScreen";
